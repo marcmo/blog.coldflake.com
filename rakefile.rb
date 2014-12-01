b2c53a548a98374ed71dcaf042b1ec1a5d40923a
Oliver Mueller <oliver.mueller@esrlabs.com>
2014-12-01 22:25:53 +0100

    deploy rake task

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,25 @@
 require 'rake/clean'
 
 CLEAN.include "logfile.log"
+
+
+desc "Deploy to Github Pages"
+task :deploy do
+  puts "## Deploying to Github Pages"
+
+  puts "## Generating site"
+  system "jekyll b"
+
+  cd "_site" do
+    system "git add -A"
+
+    message = "Site updated at #{Time.now.utc}"
+    puts "## Commiting: #{message}"
+    system "git commit -m \"#{message}\""
+
+    puts "## Pushing generated site"
+    system "git push origin gh-pages:gh-pages"
+
+    puts "## Deploy Complete!"
+  end
+end
